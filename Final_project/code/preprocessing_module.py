30d52321364afeffbe478b406f414248e1141813
DUCBUI <anhducbui272002@gmail.com>
2023-07-24 16:15:38 +0700

    preprocessing

--- Final_project/code/preprocessing_module.py
+++ Final_project/code/preprocessing_module.py
@@ -8,16 +8,19 @@ import re
 from underthesea import word_tokenize
 from transformers import AutoModel, AutoTokenizer
 import torch
+from underthesea import text_normalize
+
 
 # load model phoBert và tokenizer của model đó
 phoBert = AutoModel.from_pretrained('vinai/phobert-base')
 tokenizer = AutoTokenizer.from_pretrained("vinai/phobert-base", use_fast=False)
 
-# Xóa html tag
+
+# Xoá HTML tag
 def remove_html(text):
   return re.sub(r'<[^>]*>', '', text)
 
-# Đánh dấu spam URL (những cmt có URL thường là những cmt spam quảng cáo, có chèn các link)
+# Đánh dấu spam URL
 def check_url(text):
     pattern = re.compile(r'(http|https)://[^\s]+')
     match = pattern.search(text)
@@ -25,36 +28,51 @@ def check_url(text):
         return True
     else:
         return False
-    
-# Chuẩn hóa dấu câu
-from underthesea import text_normalize
-def convert_unicode(txt):
-  return text_normalize(txt)
 
-# Đưa văn bản về dạng viết thường
+# Chuẩn hóa Unicode và dấu câu
+uniChars = "àáảãạâầấẩẫậăằắẳẵặèéẻẽẹêềếểễệđìíỉĩịòóỏõọôồốổỗộơờớởỡợùúủũụưừứửữựỳýỷỹỵÀÁẢÃẠÂẦẤẨẪẬĂẰẮẲẴẶÈÉẺẼẸÊỀẾỂỄỆĐÌÍỈĨỊÒÓỎÕỌÔỒỐỔỖỘƠỜỚỞỠỢÙÚỦŨỤƯỪỨỬỮỰỲÝỶỸỴÂĂĐÔƠƯ"
+unsignChars = "aaaaaaaaaaaaaaaaaeeeeeeeeeeediiiiiooooooooooooooooouuuuuuuuuuuyyyyyAAAAAAAAAAAAAAAAAEEEEEEEEEEEDIIIOOOOOOOOOOOOOOOOOOOUUUUUUUUUUUYYYYYAADOOU"
+
+# Hàm tạo dict để convert định dạng cũ sang định dạng mới
+def loaddicchar():
+  dic = {}
+  char1252 = 'à|á|ả|ã|ạ|ầ|ấ|ẩ|ẫ|ậ|ằ|ắ|ẳ|ẵ|ặ|è|é|ẻ|ẽ|ẹ|ề|ế|ể|ễ|ệ|ì|í|ỉ|ĩ|ị|ò|ó|ỏ|õ|ọ|ồ|ố|ổ|ỗ|ộ|ờ|ớ|ở|ỡ|ợ|ù|ú|ủ|ũ|ụ|ừ|ứ|ử|ữ|ự|ỳ|ý|ỷ|ỹ|ỵ|À|Á|Ả|Ã|Ạ|Ầ|Ấ|Ẩ|Ẫ|Ậ|Ằ|Ắ|Ẳ|Ẵ|Ặ|È|É|Ẻ|Ẽ|Ẹ|Ề|Ế|Ể|Ễ|Ệ|Ì|Í|Ỉ|Ĩ|Ị|Ò|Ó|Ỏ|Õ|Ọ|Ồ|Ố|Ổ|Ỗ|Ộ|Ờ|Ớ|Ở|Ỡ|Ợ|Ù|Ú|Ủ|Ũ|Ụ|Ừ|Ứ|Ử|Ữ|Ự|Ỳ|Ý|Ỷ|Ỹ|Ỵ'.split('|')
+  charutf8 = "à|á|ả|ã|ạ|ầ|ấ|ẩ|ẫ|ậ|ằ|ắ|ẳ|ẵ|ặ|è|é|ẻ|ẽ|ẹ|ề|ế|ể|ễ|ệ|ì|í|ỉ|ĩ|ị|ò|ó|ỏ|õ|ọ|ồ|ố|ổ|ỗ|ộ|ờ|ớ|ở|ỡ|ợ|ù|ú|ủ|ũ|ụ|ừ|ứ|ử|ữ|ự|ỳ|ý|ỷ|ỹ|ỵ|À|Á|Ả|Ã|Ạ|Ầ|Ấ|Ẩ|Ẫ|Ậ|Ằ|Ắ|Ẳ|Ẵ|Ặ|È|É|Ẻ|Ẽ|Ẹ|Ề|Ế|Ể|Ễ|Ệ|Ì|Í|Ỉ|Ĩ|Ị|Ò|Ó|Ỏ|Õ|Ọ|Ồ|Ố|Ổ|Ỗ|Ộ|Ờ|Ớ|Ở|Ỡ|Ợ|Ù|Ú|Ủ|Ũ|Ụ|Ừ|Ứ|Ử|Ữ|Ự|Ỳ|Ý|Ỷ|Ỹ|Ỵ".split('|')
+  for i in range(len(char1252)):
+      dic[char1252[i]] = charutf8[i]
+  return dic
+
+dicchar = loaddicchar()
+def convert_unicode(text):
+    text = re.sub(
+        r'à|á|ả|ã|ạ|ầ|ấ|ẩ|ẫ|ậ|ằ|ắ|ẳ|ẵ|ặ|è|é|ẻ|ẽ|ẹ|ề|ế|ể|ễ|ệ|ì|í|ỉ|ĩ|ị|ò|ó|ỏ|õ|ọ|ồ|ố|ổ|ỗ|ộ|ờ|ớ|ở|ỡ|ợ|ù|ú|ủ|ũ|ụ|ừ|ứ|ử|ữ|ự|ỳ|ý|ỷ|ỹ|ỵ|À|Á|Ả|Ã|Ạ|Ầ|Ấ|Ẩ|Ẫ|Ậ|Ằ|Ắ|Ẳ|Ẵ|Ặ|È|É|Ẻ|Ẽ|Ẹ|Ề|Ế|Ể|Ễ|Ệ|Ì|Í|Ỉ|Ĩ|Ị|Ò|Ó|Ỏ|Õ|Ọ|Ồ|Ố|Ổ|Ỗ|Ộ|Ờ|Ớ|Ở|Ỡ|Ợ|Ù|Ú|Ủ|Ũ|Ụ|Ừ|Ứ|Ử|Ữ|Ự|Ỳ|Ý|Ỷ|Ỹ|Ỵ',
+        lambda x: dicchar[x.group()], text)
+    return text_normalize(text)
+
+# Đưa về dạng viết thường
 def to_lower_case(text):
     text = text.lower()
     return text
 
-# Xóa các ký tự không cần thiết
-def remove_unnecessary_charactor(document):
+# Xoá các ký tự không cần thiết
+def remove_unnecessary_charactor(text):
     # xóa các ký tự đặc biệt, emoji
-    document = re.sub(r'[^\s\wáàảãạăắằẳẵặâấầẩẫậéèẻẽẹêếềểễệóòỏõọôốồổỗộơớờởỡợíìỉĩịúùủũụưứừửữựýỳỷỹỵđ_]',' ',document)
+    text = re.sub(r'[^\s\wáàảãạăắằẳẵặâấầẩẫậéèẻẽẹêếềểễệóòỏõọôốồổỗộơớờởỡợíìỉĩịúùủũụưứừửữựýỳỷỹỵđ_]',' ',text)
     # xóa kí tự chứa số
-    document = re.sub(r'\w*\d\w*', '', document).strip()
+    text = re.sub(r'\w*\d\w*', '', text).strip()
     # xóa khoảng trắng thừa
-    document = re.sub(r'\s+', ' ', document).strip()
-    return document
+    text = re.sub(r'\s+', ' ', text).strip()
+    return text
 
-# Chuẩn hóa những từ lặp âm tiết
+
+# Chuẩn hoá những từ lặp âm tiết
 def remove_duplicate_characters(text):
     pattern = re.compile(r'(\w)\1{2,}')
     text = pattern.sub(r'\1', text)
     return text
 
-
 # Chuẩn hoá viết tắt
-def abbreviate(text, path = './Final_project/code/abbreviations.txt'):
+def abbreviate(text, path = './abbreviations.txt'):
     # Đọc các cặp giá trị từ file văn bản
     replacements = {}
     with open(path, 'r', encoding='UTF-8') as file:
@@ -67,12 +85,11 @@ def abbreviate(text, path = './Final_project/code/abbreviations.txt'):
     return(text)
 
 # Tách từ tiếng Việt
-def word_token(sentence):
-    return word_tokenize(sentence, format='text')
-
+def word_token(text):
+    return word_tokenize(text, format='text')
 
 # xử lý stopword
-def remove_stopwords(document, path = "./Final_project/code/stopword.txt"):
+def remove_stopwords(text, path = "./stopword.txt"):
     # stopwords = open(path)
     # stopwords = stopwords.readlines()
     # stopwords = [x.strip() for x in stopwords]
@@ -80,7 +97,7 @@ def remove_stopwords(document, path = "./Final_project/code/stopword.txt"):
     with open(path, 'r', encoding='UTF-8') as file:
         for line in file:
             stopwords.append(line.strip())
-    words = document.split(' ')
+    words = text.split(' ')
     res = list()
     for word in words:
         if word not in stopwords:
@@ -88,16 +105,9 @@ def remove_stopwords(document, path = "./Final_project/code/stopword.txt"):
 
     return ' '.join(res)
 
-# Tách một đoạn văn thành nhiều câu
-def extract_sectence_from_paragraph(paragraph):
-    sentences = []
-    for sentence in paragraph.split('.'):
-        sentences.extend(sentence.split('\n'))
-    sentences = [sentence.strip() for sentence in sentences if sentence.strip() != '']
-    return sentences
 
-# Feature extraction bằng phoBERT
-def feature_extraction(document):
+# Feature extraction phoBERT
+def feature_extraction(text):
     # Đưa từng sentence qua tokenizer của PhoBERT để convert sang dạng token index với cùng chiều dài
     # params
     MAX_SEQ_LEN = 256 # chiều dài tối đa của một câu
@@ -119,7 +129,7 @@ def feature_extraction(document):
         
         return tokenized
     
-    tokenized = [tokenize_line(document)]
+    tokenized = [tokenize_line(text)]
 
     mask = [np.where(np.array(tokenized) == 1, 0, 1)]
 
@@ -137,3 +147,10 @@ def feature_extraction(document):
     return extract_line(tokenized, mask)
 
 
+# Tách một đoạn văn thành nhiều câu
+def extract_sectence_from_paragraph(paragraph):
+    sentences = []
+    for sentence in paragraph.split('.'):
+        sentences.extend(sentence.split('\n'))
+    sentences = [sentence.strip() for sentence in sentences if sentence.strip() != '']
+    return sentences
